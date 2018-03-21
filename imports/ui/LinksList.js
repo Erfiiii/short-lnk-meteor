622cf38d9c2388ba7e06b1669d069ef38f67eda7
Erfan Hasanzadeh <er.hasanzadeh@gmail.com>
2018-03-21 12:43:00 +0330

    Initial commit

---
+++ imports/ui/LinksList.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { Tracker } from 'meteor/tracker';
+import {Session} from 'meteor/session'
+
+import { Links } from '../api/links';
+import LinksListItem from './LinksListItem';
+
+export default class LinksList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      links: []
+    };
+  }
+  componentDidMount() {
+    console.log('componentDidMount LinksList');
+    this.linksTracker = Tracker.autorun(() => {
+      Meteor.subscribe('links');
+      const links = Links.find({
+        visible:Session.get('showVisible')
+      }).fetch();
+      this.setState({ links });
+    });
+  }
+  componentWillUnmount() {
+    console.log('componentWillUnmount LinksList');
+    this.linksTracker.stop();
+  }
+  renderLinksListItems() {
+    return this.state.links.map((link) => {
+      const shortUrl = Meteor.absoluteUrl(link._id);
+      return <LinksListItem key={link._id} shortUrl={shortUrl} {...link}/>;
+    });
+  }
+  render() {
+    return (
+      <div>
+        <p>Links List</p>
+        <div>
+          {this.renderLinksListItems()}
+        </div>
+      </div>
+    );
+  }
+};
